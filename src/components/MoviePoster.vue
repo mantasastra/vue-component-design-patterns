d70cf4bd9507aa0aa8d2c46c83157c8d6f19a048
Mantas Astrauskas <mastrauskas4@gmail.com>
2021-02-12 05:51:47 +0000

    component props fundamentals

---
+++ src/components/MoviePoster.vue
@@ -0,0 +1,36 @@
+<template>
+  <section>
+    <img :src="image" alt="movie poster" />
+    <h1>{{ title }}</h1>
+    <p>{{ length }}</p>
+    <span v-if="watched">✅</span>
+  </section>
+</template>
+
+<script>
+import placeholder from "@/assets/logo.png";
+
+export default {
+  name: "MoviePoster",
+  props: {
+    image: {
+      type: String,
+      default: placeholder,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    length: {
+      type: [Number, String],
+      default: 90,
+    },
+    watched: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
+
+<style scoped></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,27 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <MoviePoster
+      :image="logo"
+      title="Framework"
+      length="42:24"
+      watched="true"
+    />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import MoviePoster from "@/components/MoviePoster";
+import logo from "@/assets/logo.png";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
-  }
+    MoviePoster,
+  },
+  data() {
+    return {
+      logo,
+    };
+  },
 };
 </script>
